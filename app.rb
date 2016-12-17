69a08a9864b0013ca11418ac68a9a245ef1d93f2
michael <andrade.michael.e@gmail.com>
2016-12-16 16:13:04 -0800

    first user story, viewing and adding stores, not passing yet

--- app.rb
+++ app.rb
@@ -6,3 +6,19 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get("/") do
   erb(:index)
 end
+
+get("/stores") do
+  @stores = Store.all()
+  erb(:stores)
+end
+
+post("/stores") do
+  store_name = params.fetch("store_name")
+  store_address = params.fetch
+  new_store = Store.new({:name => store_name, :address => store_address})
+  if new_store.save()
+    redirect to("/stores")
+  else
+    erb(:error)
+  end
+end
--- spec/shoe_distribution_integration_spec.rb
+++ spec/shoe_distribution_integration_spec.rb
@@ -1,1 +1,11 @@
 require("spec_helper")
+
+describe("As a user, I want to be able to add, update, delete and list shoe stores.", :type => :feature) do
+  it "allows the user to view and manage stores" do
+    visit('/')
+    click_on('/stores')
+    fill_in('/store_name', :with => "Shoes R Us")
+    click_on('/add new store')
+    expect(page).to have_content("Shoes R Us")
+  end
+end
