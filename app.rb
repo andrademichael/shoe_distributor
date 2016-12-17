34057a168d96bec69fb44a897eb60473e26ad4be
michael <andrade.michael.e@gmail.com>
2016-12-16 16:34:56 -0800

    adding stores spec now passing

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ end
 
 post("/stores") do
   store_name = params.fetch("store_name")
-  store_address = params.fetch
+  store_address = params.fetch("store_address")
   new_store = Store.new({:name => store_name, :address => store_address})
   if new_store.save()
     redirect to("/stores")
--- spec/shoe_distribution_integration_spec.rb
+++ spec/shoe_distribution_integration_spec.rb
@@ -3,9 +3,11 @@ require("spec_helper")
 describe("As a user, I want to be able to add, update, delete and list shoe stores.", :type => :feature) do
   it "allows the user to view and manage stores" do
     visit('/')
-    click_on('/stores')
-    fill_in('/store_name', :with => "Shoes R Us")
-    click_on('/add new store')
+    click_on('View Shoe Stores:')
+    fill_in('Store Name:', :with => "Shoes R Us")
+    fill_in('Store Address:', :with => "123 Shoe Dr")
+    click_on('Add store')
     expect(page).to have_content("Shoes R Us")
+    expect(page).to have_content("123 Shoe Dr")
   end
 end
