d64a5d097b099ca7359ed35950fe166e41f68d43
michael <andrade.michael.e@gmail.com>
2016-12-20 23:26:35 -0800

    add private method to Store to titleize name

--- spec/shoe_distribution_spec.rb
+++ spec/shoe_distribution_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
-describe("brand") do
+describe("Brand") do
   describe("#save") do
     it("capitalizes every word in a brand name") do
       test_brand = Brand.create({:name => "reebawk", :logo => nil})
       expect(test_brand.name()).to(eq("Reebawk"))
+    end
+  end
+end
+
+describe("Store") do
+  describe("#save") do
+    it("capitalizes every word in a store name") do
+      test_store = Store.create({:name => "the gip", :address => nil})
+      expect(test_store.name()).to(eq("The Gip"))
     end
   end
 end
